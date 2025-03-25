c625d164accadedb801745f42a8b56dc6acb0f66
Lobinda <lobinda123@gmail.com>
2025-03-25 12:04:13 +0800

    rr

--- src/app/2007028490/page.tsx
+++ src/app/2007028490/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import ProfileClient from "./_components/ProfileClient";
 
-export default function DynamicPage() {
-  return <ProfileClient />;
+export default function Page() {
+  return (
+    <Suspense fallback={<div className="p-4">載入中...</div>}>
+      <ProfileClient />
+    </Suspense>
+  );
 }
