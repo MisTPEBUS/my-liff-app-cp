8e39d02131bd7d64357afae2df7b650888f6f446
Lobinda <lobinda123@gmail.com>
2025-03-26 14:42:20 +0800

    dv

--- src/app/myGPTChat/page.tsx
+++ src/app/myGPTChat/page.tsx
@@ -1,13 +1,10 @@
 /* import ChatComponent from "./_components/ChatComponent/ChatComponent"; */
 import VoiceInput from "./_components/VoiceInput/VoiceInput";
 
-export const myGPTChat = () => {
+export default function MyGPTChatPage() {
   return (
     <div className="max-w-lg mx-auto mt-10 p-6 bg-white border rounded-lg shadow-lg">
-      {/*   <ChatComponent /> */}
-      <VoiceInput></VoiceInput>
+      <VoiceInput />
     </div>
   );
-};
-
-export default myGPTChat;
+}
